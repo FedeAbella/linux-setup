47393ca86edede3e63f237def0454715c18ab11e
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2023-10-07 19:48:59 -0300

    removes adding dotfiles update script to cron

--- steps/dotfiles.sh
+++ steps/dotfiles.sh
@@ -3,7 +3,3 @@
 # Clone dotfiles repository and create dotfile links
 git clone https://github.com/FedeAbella/.dotfiles.git "$HOME"/.dotfiles
 /bin/bash "$HOME"/.dotfiles/bootstrap.sh
-
-# Set up cron job to update dotfiles
-sudo touch /etc/cron.d/dotfiles-update
-echo "0 * * * * /bin/bash $HOME/.dotfiles/update.sh >/dev/null 2>&1" | sudo tee /etc/cron.d/dotfiles-update
