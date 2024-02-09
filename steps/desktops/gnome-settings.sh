14fe5e4fd765f0c214ef93ffc6ea2c7cb63b22c1
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2024-02-09 14:36:22 -0300

    Update gnome keybindings

--- steps/desktops/gnome-settings.sh
+++ steps/desktops/gnome-settings.sh
@@ -19,8 +19,9 @@ gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
 gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Shift><Alt>Tab']"
 gsettings set org.gnome.desktop.wm.keybindings switch-group "['<Super>Tab']"
 gsettings set org.gnome.settings-daemon.plugins.media-keys home "['<Super>f']" # File Manager
-gsettings set org.gnome.settings-daemon.plugins.media-keys email "['<Super>m']" # Mail client
+#gsettings set org.gnome.settings-daemon.plugins.media-keys email "['<Super>m']" # Mail client
 gsettings set org.gnome.settings-daemon.plugins.media-keys www "['<Super>b']" # Web Browser
+gsettings set org.gnome.settings-daemon.plugins.media-keys terminal "['<Super>t']" # Terminal
 gsettings set org.gnome.shell.keybindings toggle-message-tray "['<Super>v']" # Notification tray
 gsettings set org.gnome.shell.keybindings show-screenshot-ui "[]"
 gsettings set org.gnome.desktop.wm.keybindings close "['<Super>c']" # Close window
@@ -29,12 +30,12 @@ gsettings set org.gnome.desktop.wm.keybindings show-desktop "['<Super>d']" # Sho
 gsettings set org.gnome.desktop.interface icon-theme "Tela-purple-dark" # Use Tela icons
 
 # Custom keybindings
-gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
 # Tilix
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "'Tilix'"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding "'<Super>t'"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "'tilix'"
+#gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "'Tilix'"
+#gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding "'<Super>t'"
+#gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "'tilix'"
 # Flameshot
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ name "'Flameshot'"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ binding "'Print'"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ command "'flameshot gui'"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "'Flameshot'"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding "'Print'"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "'flameshot gui'"
