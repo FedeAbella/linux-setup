7c12b290ff8a4c31b38ac81b66c730970c2b8aea
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2024-04-13 13:59:10 -0300

    Add highlight install to use with ranger

--- steps/packages/ubuntu.sh
+++ steps/packages/ubuntu.sh
@@ -11,37 +11,38 @@ echo ">> Installing apt packages..."
 
 sudo apt update
 sudo apt -y install \
-alacritty \
-autojump \
-celluloid \
-dconf-editor \
-deluge \
-fd-find \
-flameshot \
-flatpak \
-fzf \
-gimp \
-gnome-shell-extensions \
-gnome-software-plugin-flatpak \
-gnome-tweaks \
-inkscape \
-lsd \
-menulibre \
-nala \
-python3-pip \
-ranger \
-repgrip \
-ruby-full \
-sayonara \
-steam \
-tldr \
-tmux \
-unzip \
-vim \
-vim-gui-common \
-yt-dlp \
-zsh \
-zsh-syntax-highlighting
+    alacritty \
+    autojump \
+    celluloid \
+    dconf-editor \
+    deluge \
+    fd-find \
+    flameshot \
+    flatpak \
+    fzf \
+    gimp \
+    gnome-shell-extensions \
+    gnome-software-plugin-flatpak \
+    gnome-tweaks \
+    highlight \
+    inkscape \
+    lsd \
+    menulibre \
+    nala \
+    python3-pip \
+    ranger \
+    repgrip \
+    ruby-full \
+    sayonara \
+    steam \
+    tldr \
+    tmux \
+    unzip \
+    vim \
+    vim-gui-common \
+    yt-dlp \
+    zsh \
+    zsh-syntax-highlighting
 
 # Link fdfind to fd
 ln -s "$(which fdfind)" "$HOME"/.local/bin/fd
@@ -67,8 +68,8 @@ sudo apt install firefox
 echo ">> Installing GitHub CLI..."
 
 type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-&& sudo apt update \
-&& sudo apt install gh -y
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg &&
+    sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg &&
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null &&
+    sudo apt update &&
+    sudo apt install gh -y
